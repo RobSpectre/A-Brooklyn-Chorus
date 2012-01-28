3e70d6e34dfaefb81f3c71e6318459138be69929
Rob Spectre <rob.spectre@gmail.com>
2012-01-28 10:05:37 -0500

    Polished up Twilio app. Fixed static file serving. Added crypto to phone numbers.

--- app.js
+++ app.js
@@ -1,13 +1,22 @@
+// Dependencies
+
 var express = require('express'), 
     app = express.createServer(), 
     io = require('socket.io').listen(app);
 
-app.use(express.bodyParser());
-
 var twilio = require('twilio');
 
-var redis = require("redis"),
+var redis = require('redis'),
     client = redis.createClient();
+
+var crypto = require('crypto');
+
+var local_settings = require('./local_settings.js');
+
+// Web app
+
+app.use(express.bodyParser());
+app.use(express.static(__dirname + '/static'));
 
 app.get('/', function(req, res){
     res.render('index.jade', {
@@ -17,25 +26,72 @@ app.get('/', function(req, res){
     });
 });
 
+
+// Twilio app
+
+function analyzeSentiment(twilio_request) {
+    console.log("Analyzing sentiment: " + twilio_request.Body);
+    var path = "http://access.alchemyapi.com/calls/text/TextGetTextSentiment?outputMode=json&apikey=" + ALCHEMY_API_KEY + "&text=" + encodeURIComponent(twilio_request.Body);
+    var request = require('request');
+    request(path , function(error, response, body){
+        if (!error && response.statusCode==200) {
+            var sentiment = JSON.parse(body);
+            console.log("Sentiment analyzed: " + twilio_request.Body);
+            console.log("Sentiment: " + sentiment.docSentiment.type);
+            console.log("Score: " + sentiment.docSentiment.score);
+            client.publish("chorus", JSON.stringify({sms: twilio_request, sentiment: sentiment}));
+            return sentiment;
+        }
+    });
+}
+
+function sms(text, res) {
+    console.log("Reply SMS " + text);
+    var r = new twilio.Twiml.Response();
+    r.append(new twilio.Twiml.Sms(text));
+    res.send(r.toString());
+}
+
+function processTwilioRequest(body) {
+    var from_hash = crypto.createHash('md5').update(body.From).digest("hex");
+    return {
+        Body: body.Body,
+        From: from_hash,
+        City: body.FromCity,
+        State: body.FromState,
+        Country: body.FromCountry,
+        Zip: body.FromZip
+    };
+}
+
 app.post('/sms', function(req,res){
     console.log("SMS received.");
     if (req.body.Body !== undefined) {
-        console.log("Analyzing sentiment of " + req.body.Body);
-        var path = "http://access.alchemyapi.com/calls/text/TextGetTextSentiment?apikey=e7bc8ff3ec4f1780eeeb64525d3d9e5a0dddd072&outputMode=json&text=" + encodeURIComponent(req.body.Body);
-        var request = require('request');
-        request(path , function(error, response, body){
-            if (!error && response.statusCode==200) {
-                body = JSON.parse(body);
-                console.log("Sentiment: " + body.docSentiment.type);
-                console.log("Score: " + body.docSentiment.score);
-                client.publish("chorus", JSON.stringify({sms: req.body, sentiment: body}));
+        var twilio_request = processTwilioRequest(req.body);
+        if (twilio_request.Body.toUpperCase() === "HELP") {
+            sms("Welcome to A Brooklyn Chorus: Text how you feel to interact with this piece.", res);
+        } else {
+            var sentiment = analyzeSentiment(twilio_request);
+            if (twilio_request.Body.length < 7) {
+                sms("This piece analyzes your sentiment - try replying again with more words for better effect.", res);
+            } else {
+                var responses = [];
+                responses.push("This piece feeds off your emotion.  The more you send, the bigger your representation in the piece.");
+                responses.push("Don't be afraid to get emotional - the more you text, the piece grows in complexity.");
+                responses.push("A Brooklyn Chorus is an emotive installation - keep texting to see more activity.");
+                responses.push("Thank you - continue texting how you feel and the piece will continue to react.");
+                responses.push("Keep the words coming - A Brooklyn Chorus is completely interactive.");
+                responses.push("The nodes you see in the piece are derived from the sentiment of text messages received. Keep them coming.");
+                responses.push("Each node is sized by how positive and negative the texter is being.  Keep texting to see the effect.");
+                responses.push("Polarizing statements make each node bigger.  Keep sending extremely positive or negative texts to see the effect.");
+                sms(responses[Math.floor(Math.random()*responses.length)], res);
             }
-        });
+        }
     }
-    var r = new twilio.Twiml.Response();
-    r.append(new twilio.Twiml.Sms('We got your message'));
-    res.send(r.toString());
 });
+
+
+// Pub/sub server
 
 io.sockets.on('connection', function(socket){
     var subscribe = redis.createClient();
