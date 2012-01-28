6932d96b4d1ac9a6c05eaf56b7416fe5ea9f6b66
Rob Spectre <rob.spectre@gmail.com>
2012-01-28 00:07:49 -0500

    Texting and sentimentalizing working.

--- app.js
+++ app.js
@@ -1,23 +1,68 @@
-var app = require('express').createServer(), 
+var express = require('express'), 
+    app = express.createServer(), 
     io = require('socket.io').listen(app);
 
+app.use(express.bodyParser());
+
+var twilio = require('twilio');
+
+var redis = require("redis"),
+    client = redis.createClient();
+
 app.get('/', function(req, res){
-    res.send('A Brooklyn Chorus.');
+    res.render('index.jade', {
+        title: 'Home',
+        io_url: 'http://localhost:5000',
+        layout: false
+    });
+});
+
+app.post('/sms', function(req,res){
+    console.log("SMS received.");
+    if (req.body.Body !== undefined) {
+        console.log("Analyzing sentiment of " + req.body.Body);
+        var path = "http://access.alchemyapi.com/calls/text/TextGetTextSentiment?apikey=e7bc8ff3ec4f1780eeeb64525d3d9e5a0dddd072&outputMode=json&text=" + encodeURIComponent(req.body.Body);
+        var request = require('request');
+        request(path , function(error, response, body){
+            if (!error && response.statusCode==200) {
+                body = JSON.parse(body);
+                console.log("Sentiment: " + body.docSentiment.type);
+                console.log("Score: " + body.docSentiment.score);
+                client.publish("chorus", JSON.stringify({sms: req.body, sentiment: body}));
+            }
+        });
+    }
+    var r = new twilio.Twiml.Response();
+    r.append(new twilio.Twiml.Sms('We got your message'));
+    res.send(r.toString());
 });
 
 io.sockets.on('connection', function(socket){
-    socket.emit('notice', { hello: 'world'});
-});
+    var subscribe = redis.createClient();
+    
+    socket.on('message', function(msg){
+        console.log(msg);
+        if (msg.action == "subscribe") {
+            console.log("Subscribe on " + msg.channel);
+            subscribe.subscribe(msg.channel);
+        }
+        if (msg.action == "unsubscribe") {
+            console.log("Unsubscribe on " + msg.channel);
+            subscribe.unsubscribe(msg.channel);
+        }
+    });
 
-app.configure('development', function(){
-    app.use(express.static(__dirname + '/static'));
-    app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
-});
+    socket.on('disconnect', function(){
+        subscribe.quit();
+    });
 
-app.configure('production', function(){
-    var oneYear = 31557600000;
-    app.use(express.static(__dirname + '/static', { maxAge: oneYear }));
-    app.use(express.errorHandler());
+    subscribe.on('message', function(channel, message){
+        console.log(channel +": " + message);
+        socket.emit('message', {
+            channel: channel,
+            data: JSON.parse(message)
+        });
+    });
 });
 
 app.listen(5000);
